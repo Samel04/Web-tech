e3a60ebc5ce1cecc0b5e5e07ad5484757928c8f6
wanaina44 <wanariffin78@gmail.com>
2026-01-07 12:45:49 +0800

    Update fetch_events.php

--- fetch_events.php
+++ fetch_events.php
@@ -1,39 +1,32 @@
-@ -0,0 +1,41 @@
 <?php
-include("../config/db.php");
+require_once "db.php";
 
 $date = $_GET['date'] ?? '';
 $category = $_GET['category'] ?? '';
 
 $sql = "SELECT * FROM events WHERE status='approved'";
 
-if ($date != '') {
+if ($date !== '') {
     $sql .= " AND event_date='$date'";
 }
-if ($category != '') {
+if ($category !== '') {
     $sql .= " AND category='$category'";
 }
 
 $sql .= " ORDER BY event_date ASC";
-$result = $conn->query($sql);
+$res = $conn->query($sql);
 
-echo "<table>";
-echo "<tr>
-        <th>Title</th>
-        <th>Date</th>
-        <th>Time</th>
-        <th>Location</th>
-        <th>Category</th>
-      </tr>";
+echo "<table>
+<tr><th>Title</th><th>Date</th><th>Time</th><th>Location</th><th>Category</th></tr>";
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
+if ($res->num_rows > 0) {
+    while ($r = $res->fetch_assoc()) {
         echo "<tr>
-                <td>{$row['title']}</td>
-                <td>{$row['event_date']}</td>
-                <td>{$row['start_time']} - {$row['end_time']}</td>
-                <td>{$row['location']}</td>
-                <td>{$row['category']}</td>
+                <td>{$r['title']}</td>
+                <td>{$r['event_date']}</td>
+                <td>{$r['start_time']} - {$r['end_time']}</td>
+                <td>{$r['location']}</td>
+                <td>{$r['category']}</td>
               </tr>";
     }
 } else {
